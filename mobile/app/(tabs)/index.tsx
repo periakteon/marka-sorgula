c09cb92263cfba1d5647636a88bebff81823a5d3
Masum Gökyüz <masumgokyuz@gmail.com>
2023-11-06 10:01:11 +0300

    add autocomplete

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,20 +1,66 @@
-import { Button } from "@ui-kitten/components";
+import { Autocomplete, AutocompleteItem } from "@ui-kitten/components";
+import { useCallback, useState } from "react";
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
+const movies = [
+  { title: "Star Wars" },
+  { title: "Back to the Future" },
+  { title: "The Matrix" },
+  { title: "Inception" },
+  { title: "Interstellar" },
+];
+
+const filter = (item: { title: string }, query: string): boolean =>
+  item.title.toLowerCase().includes(query.toLowerCase());
 
 export default function TabOneScreen() {
+  const [data, setData] = useState(movies);
+  const [value, setValue] = useState<string | undefined>(undefined);
+
+  const onSelect = useCallback(
+    (index: number): void => {
+      setValue(data[index].title);
+    },
+    [data]
+  );
+
+  const onChangeText = useCallback((query: string): void => {
+    setValue(query);
+    setData(movies.filter((item) => filter(item, query)));
+  }, []);
+
+  const renderOption = useCallback(
+    (item: { title: string }, index: number): React.ReactElement => (
+      <AutocompleteItem
+        key={index}
+        title={item.title}
+        style={styles.renderOption}
+      />
+    ),
+    []
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Marka Ara</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-      <Button>Test</Button>
+      <Autocomplete
+        placeholder="Place your Text"
+        value={value}
+        size="large"
+        status="warning"
+        placement="inner top"
+        onSelect={onSelect}
+        onChangeText={onChangeText}
+        style={styles.autoComplete}
+      >
+        {data.map(renderOption)}
+      </Autocomplete>
     </View>
   );
 }
@@ -30,8 +76,27 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   separator: {
-    marginVertical: 30,
+    marginTop: 10,
+    marginBottom: 15,
     height: 1,
-    width: "80%",
+    width: 200,
+  },
+  autoComplete: {
+    width: 200,
+    shadowColor: "gray",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    borderColor: "orange",
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+  renderOption: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+    borderRightWidth: 1,
+    borderRightColor: "#eee",
+    borderLeftWidth: 1,
+    borderLeftColor: "#eee",
   },
 });
